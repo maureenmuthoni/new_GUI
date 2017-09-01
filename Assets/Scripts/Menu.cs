fd3887dcb20cd01f2fc030a076ba9f452ce07820
maureenmuthoni <muthonimut@gmail.com>
2017-09-01 15:52:21 +1000

    All changes made in game and menu scene

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,73 +1,170 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
-	[Header("Keys")]
-	public KeyCode forward;
-	public KeyCode backward;
-	public KeyCode left;
-	public KeyCode right;
-	public KeyCode jump;
-	public KeyCode crouch;
-	public KeyCode interact;
-	public KeyCode sprint;
-	public KeyCode holdingKey;
+    [Header("Screen Elements")]
+    public bool showOptions;
+    public int scrW, scrH;
+
+    [Header("Keys")]
+    public KeyCode forward;
+    public KeyCode backward;
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode jump;
+    public KeyCode crouch;
+    public KeyCode interact;
+    public KeyCode sprint;
+    public KeyCode holdingKey;
+
+    [Header("Resolutions and Screen Elements")]
+    public int index;
+    public bool showRes;
+    public bool fullScreenToggle;
+    public int[] resX, resY;
+    private Vector2 scrollPosRes;
+
+    [Header("Other References")]
+    public AudioSource mainMusic;
+    public float volumeSlider;
+    public bool muteToggle;
+    public Light brightness;
+    public float brightnessSlider;
+    public float holdingVolume;
+
+    void Start()
+    {
+        scrW = Screen.width / 16;
+        scrH = Screen.height / 9;
+        //brightness
+        brightness = GameObject.FindGameObjectWithTag("Sun").GetComponent<Light>();
+        //mainMusic
+        mainMusic = GameObject.Find("MenuMusic").GetComponent<AudioSource>();
+        volumeSlider = mainMusic.volume;
+        brightnessSlider = brightness.intensity;
+    }
+
+    void Update()
+    {
+        if (brightness != null)
+        {
+
+            brightness.intensity = brightnessSlider;
+        }
+    }
+    void OnGUI()
+    {
+        if (!showOptions)//if we are on our menu
+        {
+            GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "");//background box
+            GUI.Box(new Rect(4 * scrW, 0.25f * scrH, 8 * scrW, 2 * scrH), "Jaymies Game Of Awesome");//Title
+            //Buttons
+            if (GUI.Button(new Rect(6 * scrW, 4 * scrH, 4 * scrW, scrH), "Play"))
+            {
+                SceneManager.LoadScene(1);
+            }
+            if (GUI.Button(new Rect(6 * scrW, 5 * scrH, 4 * scrW, scrH), "Options"))
+            {
+                showOptions = true;
+            }
+            if (GUI.Button(new Rect(6 * scrW, 6 * scrH, 4 * scrW, scrH), "Exit"))
+            {
+                Application.Quit();
+            }
+        }
+        if (showOptions)//if we are in options
+        {
+            if (scrW != Screen.width / 16)
+            {
+                scrW = Screen.width / 16;
+                scrH = Screen.height / 9;
+            }
+
+            GUI.Box(new Rect(0, 0, Screen.width, Screen.height), "");//background box
+            int i = 0;
+            GUI.Box(new Rect(4 * scrW, 0.25f * scrH, 8 * scrW, 2 * scrH), "Options");//Title
+
+            GUI.Box(new Rect(0.5f * scrW, 3 * scrH + (i * 0.75f * scrH), 1.5f * scrW, 0.75f * scrH), "Volume");
+            volumeSlider = GUI.HorizontalSlider(new Rect(2 * scrW, 3.25f * scrH + (i * 0.75f * scrH), 2f * scrW, 0.25f * scrH), volumeSlider, 0, 1);
+
+            if (GUI.Button(new Rect(4f * scrW, 3 * scrH + (i * (scrH * 0.5f)), 1.75f * scrW, 0.5f * scrH), "Mute")) // Label
+            {
+                //ToggleVolume();
+            }
+            i++;
+            GUI.Box(new Rect(0.5f * scrW, 3 * scrH + (i * 0.75f * scrH), 1.5f * scrW, 0.75f * scrH), "Brightness");
+            brightnessSlider = GUI.HorizontalSlider(new Rect(2 * scrW, 3.25f * scrH + (i * 0.75f * scrH), 2f * scrW, 0.25f * scrH), brightnessSlider, 0, 1);
+
+            #region Resolution and Screen
+            i++;
+
+            if (GUI.Button(new Rect(0.5f * scrW, 3 * scrH + (i * 0.75f * scrH), 1.5f * scrW, 0.5f * scrH), "Resolutions"))
+            {
+                showRes = !showRes;
+            }
+            if (GUI.Button(new Rect(2f * scrW, 3 * scrH + (i * 0.75f * scrH), 1.5f * scrW, 0.5f * scrH), "Fullscreen"))
+            {
+                FullScreenToggle();
+            }
+            i++;
+            i++;
+            if (showRes)
+            {
+                GUI.Box(new Rect(0.25f * scrW, 3 * scrH + (i * (scrH * 0.5f)), 1.75f * scrW, 3.5f * scrH), "");
+
+                scrollPosRes = GUI.BeginScrollView(new Rect(0.25f * scrW, 3 * scrH + (i * (scrH * 0.5f)), 1.75f * scrW, 3.5f * scrH), scrollPosRes, new Rect(0, 0, 1.75f * scrW, 3.5f * scrH));
+
+                for (int resSize = 0; resSize < resX.Length; resSize++)
+                {
+                    if (GUI.Button(new Rect(0f * scrW, 0 * scrH + resSize * (scrH * 0.5f), 1.75f * scrW, 0.5f * scrH), resX[resSize].ToString() + "x" + resY[resSize].ToString()))
+                    {
+                        Screen.SetResolution(resX[resSize], resY[resSize], fullScreenToggle);
+                        showRes = false;
 
 
-	[Header("Resolutions")]
-	public int index;
-	public int[] resX, resY;
+                    }
+                }
+                GUI.EndScrollView();
+            }
 
 
+            #endregion
 
-	[Header("References")]
-	public GameObject menu, options, pauseMenu, pauseOptions; // To allow public access to these GameObjects ("menu", "options", "pauseMenu", "pauseOptions") in Unity and C#
-	public AudioSource mainMusic; // Allow public access to the AudioSource which has been named "mainMusic"
-	public float volumeSlider, brightnessSlider; // Allow public access to the Sliders, named "volumeSlider" and "brightnessSlider"
-	public sliderbrightnessSlider; // Allow public access to Light, named "brightness"
+        }
+    }
 
-	void Start()
-	{
-
-	scrW = Screen.width / 16;
-	scrH = Screen.height/ 9;
-	}
-	
-	void OnGUI()
-	{
-		if(showOptions) // if we are on menu
-		if(scrw != ScreenWidth/16)
-		scrW = Screen.width / 16;
-		scrH = Screen.height/ 9;
-	}
-		
-
-				GUI.Box (new Rect (0, 0, Screen.width, Screen.height), ""); // background box
-			int i = 0
-			GUI.Box (new Rect(4*scrW,0.25f*scrH,8*scrw,2*scrH), "I Love Programming" //Title
-				// Buttons
-
-				if (GUI.Button(new Rect(6*scrW,4*scrH,4*scrw,scrH),"Play"))
-				{
-				SceneManager.LoadScene(1);
-				}
-			if(GUI.Button(new Rect(6*scrW,5*scrH,4*scrw,scrH),"Options"))
-					{
-						showOptions = true;
-					}
-					
-
-				if (GUI.Button(new Rect(6*scrW,6*scrH,4*scrw,scrH),"Exit"))
-						{
-							Application.Quit();
-				}
-				scrW = Screen.width /16;
-				scrH = Screen.height /19;
-				GUI.Box (new Rect (0, 0, Screen.width, Screen.height), ""); // background box		
-				int i = 0
-
-				if (GUI.Button(new Rect(6*scrW,5*scrH,4*scrw,scrH),"Options"))
-
-				}
+    bool ToggleVolume()
+    {
+        if (muteToggle == true)
+        {
+            muteToggle = false;
+            volumeSlider = holdingVolume;
+            return false;
+        }
+        else
+        {
+            muteToggle = true;
+            holdingVolume = volumeSlider;
+            volumeSlider = 0;
+            mainMusic.volume = 0;
+            return true;
+        }
+    }
+    bool FullScreenToggle()
+    {
+        if (fullScreenToggle)
+        {
+            fullScreenToggle = false;
+            Screen.fullScreen = false;
+            return false;
+        }
+        else
+        {
+            fullScreenToggle = true;
+            Screen.fullScreen = true;
+            return true;
+        }
+    }
+}
---
+++ Assets/Scripts/TimerClockHardCode.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerClockHardCode : MonoBehaviour
+{
+    public float timer; // Set this to the time you want in seconds + 1 second for the pc load start
+
+    // Use this for initiallization
+    void Start()
+    {
+        Time.timeScale = 1;
+
+    }
+   // This is called once per frame
+ void Update()
+    {
+        if (timer > 0) // if we are greater than 0
+        {
+            timer -= Time.deltaTime; // count down.... this may take us below 0
+        }
+    }
+
+void LateUpdate()
+{
+    if(timer < 0)
+    {
+        timer = 0; // Sets us back to 0
+    }
+}     
+void OnGUI()
+{
+  //Screen by aspect ratio 16.9 
+    float scrW = Screen.width / 16;
+    float scrH = Screen.height / 9;
+
+    int mins = Mathf.FloorToInt(timer / 60);
+ int secs = Mathf.FloorToInt(timer - mins * 60);
+
+    string clockTime = string.Format("{0:0}:{1:00}", mins, secs);
+        GUI.Box(new Rect(scrW, scrH, scrW, scrH), clockTime); // dISPLAY CLOCK
+}
+	
+ 
+}
